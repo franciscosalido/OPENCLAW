fc7a0d7a232066d9fc8db7a6be99affe09d60ff8
Francisco Salido <franciscosalido@gmail.com>
2026-05-13 21:58:02 -0300

    Use public evaluation API in metric tests RC-6

--- tests/unit/test_eval_metrics.py
+++ tests/unit/test_eval_metrics.py
@@ -6,11 +6,17 @@ import math
 import unittest
 from collections.abc import Sequence
 
-from evaluation.eval_latency import latency_percentiles
-from evaluation.eval_mrr import mean_reciprocal_rank, reciprocal_rank
-from evaluation.eval_ndcg import dcg_at_k, ndcg_at_k
-from evaluation.eval_precision import mean_precision_at_k, precision_at_k
-from evaluation.eval_recall import mean_recall_at_k, recall_at_k
+from evaluation import (
+    dcg_at_k,
+    latency_percentiles,
+    mean_precision_at_k,
+    mean_recall_at_k,
+    mean_reciprocal_rank,
+    ndcg_at_k,
+    precision_at_k,
+    recall_at_k,
+    reciprocal_rank,
+)
 
 
 class PrecisionAtKTests(unittest.TestCase):
